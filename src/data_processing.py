f30ce97214394e6975fc711b891af1f98b225c20
Ez-C99 <ezchamba@gmail.com>
2023-10-23 19:25:23 +0100

    setup for testing code

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import pandas as pd
 from datetime import datetime
 
@@ -24,6 +23,7 @@ def extract(data_path=DATA_PATH):
         raise ValueError(f"Failed to load data from {data_path}: {e}")
     return data
 
+
 def export_snapshot(data, snapshot_folder, snapshot_name):
     """
     Export snapshot of data to the required destination.
@@ -33,28 +33,33 @@ def export_snapshot(data, snapshot_folder, snapshot_name):
     snapshot_folder (str): The path of the folder to save to
     snapshot_name (str): The name to be assigned to the file with a timestamp
 
-    Returns
+    Returns:
+    str: The path of the folder the snapshot has been saved to
+
+    Raises:
+    IOError: If there is an issue writing to the specified file path.
     """
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     snapshot_path = os.path.join(snapshot_folder, f'{snapshot_name}_{timestamp}.csv')
     data.to_csv(snapshot_path, index=False)
     return snapshot_path  # Returning the path can be useful for logging or further processing
 
+
 def load_from_staging(staging_folder, file_name):
     """
     Load snapshot of data from the required destination.
 
     Parameters:
-    data (pd.DataFrame): The input data.
-    snapshot_folder (str): The path of the folder to load from
-    snapshot_name (str): The name of the file to load
+    staging_folder (str): The path of the folder to load from.
+    file_name (str): The name of the file to load.
 
-    Returns
-    data (pd.DataFrame): The data being 
+    Returns:
+    pd.DataFrame: The loaded data.
     """
     file_path = os.path.join(staging_folder, file_name)
     data = pd.read_csv(file_path)
     return data
+
 
 def deduplicate(data):
     """
@@ -65,12 +70,16 @@ def deduplicate(data):
     
     Returns:
     pd.DataFrame: The deduplicated data.
+
+    Raises:
+    ValueError: If the input data is empty.
     """
     if data.empty:
         raise ValueError("Input data is empty")
     
     deduplicated_data = data.drop_duplicates(subset=['id', 'created_at'])
     return deduplicated_data
+
 
 def rank_users(data):
     """
@@ -81,6 +90,9 @@ def rank_users(data):
     
     Returns:
     pd.DataFrame: The data with an added 'age_group_rank' column.
+
+    Raises:
+    ValueError: If the input data is empty.
     """
     if data.empty:
         raise ValueError("Input data is empty")
@@ -95,6 +107,7 @@ def rank_users(data):
     data_copy['age_group_rank'] = data_copy.groupby('age_group').cumcount() + 1
 
     return data_copy
+
 
 def get_top_user_per_age_group(data):
     """
@@ -119,6 +132,7 @@ def get_top_user_per_age_group(data):
                 .groupby('age_group')\
                 .head(1)[['id', 'email', 'age_group']]
 
+
 def flatten_widget_list(data):
     """
     Flatten the widget_list column.
@@ -128,12 +142,16 @@ def flatten_widget_list(data):
     
     Returns:
     pd.DataFrame: The data with the 'widget_list' column exploded.
+
+    Raises:
+    ValueError: If the input data is empty.
     """
     if data.empty:
         raise ValueError("Input data is empty")
 
     data_exploded = data.explode('widget_list')
     return data_exploded
+
 
 def convert_unsupported_data_types(data):
     """
@@ -144,9 +162,16 @@ def convert_unsupported_data_types(data):
 
     Returns:
     pd.DataFrame: The data with unsupported data types converted to strings.
+
+    Raises:
+    ValueError: If the input data is empty.
     """
+    if data.empty:
+        raise ValueError("Input data is empty")
+
     converted_data = data.applymap(lambda x: str(x) if isinstance(x, (list, dict)) else x)
     return converted_data
+
 
 def extract_widget_info(data):
     """
@@ -157,6 +182,9 @@ def extract_widget_info(data):
     
     Returns:
     pd.DataFrame: The data with added 'widget_name' and 'widget_amount' columns.
+
+    Raises:
+    ValueError: If the input data is empty.
     """
     if data.empty:
         raise ValueError("Input data is empty")
